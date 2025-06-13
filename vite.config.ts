a14481f11128c33f075a3ef833298998a1dbc84c
zachczx <zixian.chen@gmail.com>
2025-06-13 23:33:36 +0800

    Added images and enhanced:img

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		port: 5273
-	},
-	plugins: [tailwindcss(), sveltekit()]
+	// server: {
+	// 	port: 5273
+	// },
+	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
 });
