86250f4e53f87887f6a73714bfe3e2450fd31379
zachczx <zixian.chen@gmail.com>
2025-07-08 00:08:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// server: {
-	// 	port: 5273
-	// },
 	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
 });
